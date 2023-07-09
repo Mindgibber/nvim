7f09fc8318174bd34ac17f5b3fea9140131b159c
Mindgibber <mindgibber@sapo.pt>
2023-07-09 01:41:52 +0100

    Config

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -5,6 +5,7 @@ lspconfig.zls.setup {}
 lsp.preset("recommended")
 
 lsp.ensure_installed({
+    "lua_ls",
 })
 
 -- Fix Undefined global 'vim'
@@ -49,8 +50,8 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
+  vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
